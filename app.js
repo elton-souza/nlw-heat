e7ccc1727382ffcaad2fbf8b61bd03d1fea7e146
Elton Souza <elton.souza1515@gmail.com>
2021-10-22 10:33:16 -0300

    finalizando projeto

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ const linksSocialMedia = {
     instagram : "notle.py",
     twitter : "notlesz"
 }
+
 function changeSocialMediaLinks(){
-    document.getElementById('github').href = `https://github.com/${linksSocialMedia.github}`
-    
     for(let li of socialLinks.children){
         const social = li.getAttribute('class')
         li.children[0].href = `https://www.${social}.com/${linksSocialMedia[social]}`
     }
 }
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        userPhoto.src = data.avatar_url
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userLogin.textContent = data.login
+    })
+}
+
 
 changeSocialMediaLinks()
+getGitHubProfileInfos()
